e0be325b81b26f80d2b6897ea0d41989217ff284
v.kisel <v.kisel@corp.mail.ru>
2021-05-17 16:51:37 +0300

    some fix

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('hello world');
+import express from 'express';
+import { PORT } from '@constants';
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Listening at ${PORT} port`)
+});
